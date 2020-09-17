f431c05721a57d6a6ad7aba11cbba6c9351fd820
Alejo Totaro <alejocs1998@gmail.com>
2020-09-16 21:46:32 -0300

    Commit Initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,37 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './components/home/home.component';
+import {ProductComponent} from './components/product/product.component';
+import {CartComponent} from './components/cart/cart.component';
+import {CheckoutComponent} from './components/checkout/checkout.component';
+import {GraciasComponent} from './components/gracias/gracias.component';
+import {RegistroComponent} from './components/registro/registro.component';
+import {LoginComponent} from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component:HomeComponent
+  },
+  {
+    path: 'producto/:id', component: ProductComponent
+  },
+  {
+    path: 'carro', component: CartComponent
+  },
+  {
+    path: 'pago', component: CheckoutComponent
+  },
+  {
+    path: 'gracias', component: GraciasComponent
+  },
+
+  {
+    path:'registro', component:RegistroComponent
+  },
+  {
+    path:'login', component:LoginComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,39 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CartComponent } from './components/cart/cart.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductComponent } from './components/product/product.component';
+import { GraciasComponent } from './components/gracias/gracias.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CartComponent,
+    CheckoutComponent,
+    HomeComponent,
+    ProductComponent,
+    GraciasComponent,
+    LoginComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule, /* Agregado */
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
